58c64e9617906dd1ecc22b9f753b4c283433f852
Anna Belousova <belusovanna@gmail.com>
2024-11-26 14:33:03 +0500

    add Allure

--- src/test/java/com/example/teamcity/ui/BaseUiTest.java
+++ src/test/java/com/example/teamcity/ui/BaseUiTest.java
@@ -1,12 +1,15 @@
 package com.example.teamcity.ui;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.example.teamcity.BaseTest;
 import com.example.teamcity.api.config.Config;
 import com.example.teamcity.api.enums.Endpoint;
 import com.example.teamcity.api.generators.TestDataStorage;
 import com.example.teamcity.api.models.User;
 import com.example.teamcity.ui.pages.LoginPage;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -18,12 +21,19 @@ import static com.codeborne.selenide.Configuration.*;
 public class BaseUiTest extends BaseTest {
     @BeforeSuite(alwaysRun = true)
     public void setupUiTest() {
-        browser = Config.getProperty("browser");
-        baseUrl = "http://" + Config.getProperty("host");
-        remote = Config.getProperty("remote");
-        browserSize = Config.getProperty("browserSize");
+        Configuration.browser = Config.getProperty("browser");
+        Configuration.baseUrl = "http://" + Config.getProperty("host");
+        // НЕТ ПИШИТЕ UI ТЕСТЫ С ЛОКАЛЬНЫМ БРАУЗЕРОМ
+        // А ПОТОМ ЗАПУСКАЕТЕ НА REMOTE BROWSER
+        Configuration.remote = Config.getProperty("remote");
+        Configuration.browserSize = Config.getProperty("browserSize");
 
-        browserCapabilities.setCapability("selenoid:options", Map.of("enableVNC", true, "enableLog", true));
+        Configuration.browserCapabilities.setCapability("selenoid:options", Map.of("enableVNC", true, "enableLog", true));
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+                .includeSelenideSteps(true));
     }
 
     protected TestDataStorage testDataStorage;
